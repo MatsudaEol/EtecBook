25e576ff4f74a82dc4979a013f5fa3b6ba1bb07b
MatsudaEol <110265861+MatsudaEol@users.noreply.github.com>
2023-10-31 22:22:50 -0300

    Login

--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(){
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Enviar os dados a API
+    } else {
+      // Exibir mensagem de erro
+    }
+  }
 }
